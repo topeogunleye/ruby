e40fd4994fa17bdff2e05f4584a61b51323d9cd5
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-05 18:51:04 +0100

    Convert str to interger

--- ruby.rb
+++ ruby.rb
@@ -1,3 +1,10 @@
 puts "Enter Your Name: "
 name = gets.chomp()
 puts ("Hello " + name + ", you are cool!")
+
+puts "Enter a number: "
+num1 = gets.chomp()
+puts "Enter another number"
+num2 = gets.chomp()
+
+puts (num1.to_i + num2)
