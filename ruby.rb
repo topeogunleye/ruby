f2ba077345bc4088235c9932b5e28a5a2a0e4832
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-05 19:22:36 +0100

    Add Comments To Code

--- ruby.rb
+++ ruby.rb
@@ -1,22 +1,35 @@
+#--------------------------------------
+# Prompt from user for a string
 # puts "Enter Your Name: "
 # name = gets.chomp()
 # puts ("Hello " + name + ", you are cool!")
 
+#--------------------------------------
+# Prompt from user for a number
 # puts "Enter a number: "
 # num1 = gets.chomp()
 # puts "Enter another number"
 # num2 = gets.chomp()
 
+# -------------------------------------
+# Convert string to interger
 # puts (num1.to_i + num2.to_i)
+# -------------------------------------
+# Convert string to floating point
 # puts (num1.to_f + num2.to_f)
 
-puts "Enter a color: "
-color = gets.chomp()
-puts "Enter a plural noun: "
-plural_noun = gets.chomp()
-puts "Enter a celebrity: "
-celebrity = gets.chomp()
+# ------------------------------------------
+# MatLib Game
 
-puts ("Roses are " + color)
-puts (plural_noun + " are blue")
-puts ("I love " + celebrity)
+# puts "Enter a color: "
+# color = gets.chomp()
+# puts "Enter a plural noun: "
+# plural_noun = gets.chomp()
+# puts "Enter a celebrity: "
+# celebrity = gets.chomp()
+
+# puts ("Roses are " + color)
+# puts (plural_noun + " are blue")
+# puts ("I love " + celebrity)
+
+# Arrays
