04d641fa70e6ab7c7c5a521e0d96e0eff1f1226c
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-05 19:14:11 +0100

    Add Matlib Game with ruby

--- ruby.rb
+++ ruby.rb
@@ -2,10 +2,21 @@
 # name = gets.chomp()
 # puts ("Hello " + name + ", you are cool!")
 
-puts "Enter a number: "
-num1 = gets.chomp()
-puts "Enter another number"
-num2 = gets.chomp()
+# puts "Enter a number: "
+# num1 = gets.chomp()
+# puts "Enter another number"
+# num2 = gets.chomp()
 
-puts (num1.to_i + num2.to_i)
-puts (num1.to_f + num2.to_f)
+# puts (num1.to_i + num2.to_i)
+# puts (num1.to_f + num2.to_f)
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
