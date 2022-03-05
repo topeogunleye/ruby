80a0e85d877fcbb60df9f4c9d28548b8ced3c2b7
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-05 18:52:13 +0100

    Comment out prompt for  user information

--- ruby.rb
+++ ruby.rb
@@ -1,10 +1,10 @@
-puts "Enter Your Name: "
-name = gets.chomp()
-puts ("Hello " + name + ", you are cool!")
+# puts "Enter Your Name: "
+# name = gets.chomp()
+# puts ("Hello " + name + ", you are cool!")
 
 puts "Enter a number: "
 num1 = gets.chomp()
 puts "Enter another number"
 num2 = gets.chomp()
 
-puts (num1.to_i + num2)
+puts (num1.to_i + num2.to_i)
