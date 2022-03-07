588dc506adbc1501d1ab94825f777b856bf7059b
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 11:40:07 +0100

    Define methods for animal 1 and 2

--- ruby.rb
+++ ruby.rb
@@ -164,18 +164,37 @@ states = {
 
 # -------------------------------------
 # Constructor with optional parameters
+# class Animal
+#   def initialize(number_of_legs, name = "Unknown")
+#     @id = Random.rand(1..1000)
+#     @name = name
+#     @number_of_legs = number_of_legs
+#   end
+# end
+
+# animal_1 = Animal.new(4, "Rex")
+# animal_2 = Animal.new(8)
+
+
+# animal_1
+# animal_2
+
+# -------------------------------------
+# Method definition
 class Animal
   def initialize(number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
   end
+
+  def speak
+    "Bla bla bla"
+  end
 end
 
 animal_1 = Animal.new(4, "Rex")
 animal_2 = Animal.new(8)
 
-
-animal_1
-animal_2
-
+animal_1.speak
+animal_2.speak
