f6db456dca142d38961ab6c05b9b080f31dd1db3
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 11:29:17 +0100

    Practise constructor and instance vars

--- ruby.rb
+++ ruby.rb
@@ -124,3 +124,18 @@ animal_2 = Animal.new
 
 animal_1
 animal_2
+
+# Constructor and instance vars
+class Animal
+  def initialize
+    @id = Random.rand(1..1000)
+    @name = "Rex"
+    @number_of_legs = 4
+  end
+end
+
+animal_1 = Animal.new
+animal_2 = Animal.new
+
+animal_1
+animal_2
