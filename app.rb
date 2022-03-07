98f433f6b0ac3a420ddd6b5bf4cc787d7cba7060
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 14:48:17 +0100

    Practise abstaction

--- app.rb
+++ app.rb
@@ -11,3 +11,10 @@ animal_2 = Animal.new("cat", 8)
 animal_2.name
 animal_2.name = "Fluffy"
 animal_2.name
+
+# Abstraction
+animal_1 = Animal.new("dog", 4, "Rex")
+animal_2 = Animal.new("spider", 8, "Wilma")
+
+animal_1.speak()
+animal_2.speak()
