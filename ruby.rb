0b3fc825e72f1a3f0a7c97fc7f7acc9d179afd60
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 11:36:49 +0100

    Practise constructor with optional parameters

--- ruby.rb
+++ ruby.rb
@@ -144,20 +144,38 @@ states = {
 
 # -------------------------------------
 # Constructor with parameters
+# class Animal
+#   def initialize(name, number_of_legs)
+#     @id = Random.rand(1..1000)
+#     @name = name
+#     @number_of_legs = number_of_legs
+#   end
+# end
+
+# animal_1 = Animal.new("Rex", 4)
+# animal_2 = Animal.new("Bob", 8)
+
+# animal_3 = Animal.new
+# animal_4 = Animal.new("Rex")
+
+
+# animal_1
+# animal_2
+
+# -------------------------------------
+# Constructor with optional parameters
 class Animal
-  def initialize(name, number_of_legs)
+  def initialize(number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
   end
 end
 
-animal_1 = Animal.new("Rex", 4)
-animal_2 = Animal.new("Bob", 8)
-
-animal_3 = Animal.new
-animal_4 = Animal.new("Rex")
+animal_1 = Animal.new(4, "Rex")
+animal_2 = Animal.new(8)
 
 
 animal_1
 animal_2
+
