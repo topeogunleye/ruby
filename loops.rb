f5eb5eeca067e72b2abd60412cc190aadb5ef255
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-16 18:13:31 +0100

    Add for loops and comment out while loop

--- loops.rb
+++ loops.rb
@@ -1,6 +1,14 @@
-# While loops
-index = 1
-while index <= 10
-  puts index
-  index += 1
+# while loops
+# index = 1
+# while index <= 10
+#   puts index
+#   index += 1
+# end
+
+# for loops
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+for friend in friends
+  puts "Hello, #{friend}!"
 end
+
