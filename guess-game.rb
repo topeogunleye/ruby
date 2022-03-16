5c7e11c5ff167f56068e8860cb9f910898ead2b2
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-16 18:02:00 +0100

    Improve guess game

--- guess-game.rb
+++ guess-game.rb
@@ -1,10 +1,22 @@
 
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
-while secret_word != guess
-  puts "Guess the secret word!"
-  guess = gets.chomp()
+while guess != secret_word && !out_of_guesses
+  if guess_count < guess_limit
+    puts "Guess the secret word!"
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    out_of_guesses = true
+  end
 end
 
-puta "You got it!"
+if out_of_guesses && guess != secret_word
+  puts "You lose!"
+else
+  puts "You win!"
+end
