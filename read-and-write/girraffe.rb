c0e5bc0ecb95018d26737f7e7f138b08a847854c
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-17 06:36:37 +0100

    Loop with for in and readllines

--- read-and-write/girraffe.rb
+++ read-and-write/girraffe.rb
@@ -1,5 +1,11 @@
+# File.open("employees.txt", "r") do |file|
+#   file.each_line do |line|
+#     puts line
+#   end
+# end
+
 File.open("employees.txt", "r") do |file|
-  file.each_line do |line|
+  for line in file.readlines
     puts line
   end
 end
