b1298a1c6626202a497cdf84b3473bf36269fe46
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-20 10:15:23 +0100

    Add Classes and Objects with getters and setters

--- Classes-and-Objects/girrafe.rb
+++ Classes-and-Objects/girrafe.rb
@@ -1,8 +1,32 @@
+# classes and objects with classical getter and setter
 class Book
-  attr_accessor :title, :author, :pages
   def initialize(title, author, pages)
     @title = title
     @author = author
+    @pages = pages
+  end
+
+  def title
+    @title
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def author
+    @author
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def pages
+    @pages
+  end
+
+  def pages=(pages)
     @pages = pages
   end
 end
@@ -11,3 +35,21 @@ book = Book.new("Harry Potter", "JK Rowling", 400)
 puts book.title
 puts book.author
 puts book.pages
+
+# classes and objects with shortcut getter and setter
+# class Book
+#   attr_accessor :title, :author, :pages
+#   def initialize(title, author, pages)
+#     @title = title
+#     @author = author
+#     @pages = pages
+#   end
+# end
+
+# book = Book.new("Harry Potter", "JK Rowling", 400)
+# puts book.title
+# puts book.author
+# puts book.pages
+
+
+
